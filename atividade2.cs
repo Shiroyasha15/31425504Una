2bbb48247c0780dcc3514f5fc8bb3cb7332b55b6
Shiroyasha15 <42787914+Shiroyasha15@users.noreply.github.com>
2018-09-24 11:31:10 -0300

    Add files via upload

---
+++ atividade2.cs
@@ -0,0 +1,38 @@
+﻿
+public class ContaPoupanca
+{
+    static readonly int TaxaDeJurosAnual = 4 / 100;
+    static readonly int SaldoPoupanca = 0;
+
+    private int CalcularJurosMensal(int SaldoPoupanca, int TaxaDeJurosAnual)
+    {
+
+        return (SaldoPoupanca * TaxaDeJurosAnual) / 12;
+
+    }
+
+    private static int AlterarTaxaDeJuros()
+    {
+
+
+    }
+
+
+
+}
+
+    
+}
+
+using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
